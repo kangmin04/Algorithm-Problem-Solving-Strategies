af23eab637d974f6257fdf8cf18caebab68de88f
kangmin04 <146157505+kangmin04@users.noreply.github.com>
2026-06-19 22:19:39 +0900

    Study basic concepts of C++ and initilize c++ compiler settings

---
+++ 01-c-basic/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, Mac C++ World!" << endl;
+    
+    string name;
+    cout << "이름을 입력하세요: ";
+    cin >> name;
+    cout << "반갑습니다, " << name << "님!" << endl;
+    
+    return 0;
+}
